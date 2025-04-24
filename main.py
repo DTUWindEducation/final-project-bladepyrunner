b6409b556a2588cdbcb6ca97bd34c9e5ad68cd33
MaxRosendahl <90492387+MaxRosendahl@users.noreply.github.com>
2025-04-24 12:42:35 +0200

    Added init file and created functions for loading and calculating wind speed

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import xarray as xr
 from pathlib import Path
 import datetime
 from cftime import num2date
+from src.__init__ import load_nc_files 
+from src.__init__ import compute_speed_direction
 
-# Define the folder path containing the NetCDF files
-folder_path = Path('inputs\WindData')
-datasets = []  # List to store datasets
+# Load the NetCDF file
+data_dir = Path('inputs/WindData')
+ds = load_nc_files(data_dir)
 
-for file in folder_path.iterdir():
-    ds = xr.open_dataset(file)  # Open each NetCDF file
-    datasets.append(ds)  # Store dataset in list
+u10 = ds['u10'].values
+v10 = ds['v10'].values
 
-# Concatenate along a specific dimension (e.g., 'time' if it exists)
-combined_ds = xr.concat(datasets, dim='valid_time')
+wind_speed, wind_dir = compute_speed_direction(u10, v10)
 
-# Save or process the combined dataset
-# combined_ds.to_netcdf("merged_output.nc")  # Save to a new NetCDF file
-
-# Display the combined dataset
-print(combined_ds)
+location_1 = wind_speed[wind_speed['location'] == (7.75, 55.5)]
