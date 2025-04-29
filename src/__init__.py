30755eb3afb2fab824b6c7d3909ff0784d3c031c
Crisfengu <c.fenteg@gmail.com>
2025-04-29 11:23:25 +0200

    Included the code of Rodrigo and Max into the architecture

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-# include here the functions
+from .Load_Data import load_nc_files, compute_speed_direction
+from .Interpolate import interpolate_with_xarray
+from .Power_Law_Profile import power_law_calculation
+from .weibull import fit_weibull
+from .plots import plot_weibuill
